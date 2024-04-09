ffa9dd30b3460d79c2d63732801e58a49554e5c9
abderahman1337 <166234571+abderahman1337@users.noreply.github.com>
2024-04-09 17:16:34 +0100

    update

--- resources/views/vehicule.blade.php
+++ resources/views/vehicule.blade.php
@@ -7,7 +7,7 @@
             <!-- Carousel wrapper -->
             <div class="relative h-56 overflow-hidden rounded-lg md:h-[500px]">
                 @foreach ($vehicule->images as $vehiculeImage)
-                <div class="hidden duration-700 ease-in-out" data-carousel-item="{{$loop->first?'active':''}}">
+                <div class="hidden duration-700 ease-in-out" data-carousel-item="{{$vehicule->image == $vehiculeImage->image?'active':''}}">
                     <img src="{{$vehiculeImage->getImage()}}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                 </div>
                 @endforeach
