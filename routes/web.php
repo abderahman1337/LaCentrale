4a75ace1f3b1d3b6bd01e273eb7feac91136794c
abderahman1337 <166234571+abderahman1337@users.noreply.github.com>
2024-04-11 00:08:33 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::middleware('RateLimit')->group(function (){
 
 });
 
-Route::middleware(['auth', 'CheckRole:customer'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
